0d332ad401ae63bd4092dacca5661049e8f137db
christinekeni <christine.keni@strathmore.edu>
2024-11-27 10:15:08 +0300

    builds

--- data_monitoring.asm
+++ data_monitoring.asm
@@ -1,6 +1,7 @@
 section .data
     prompt db "Enter a number: ", 0
     result_msg db "Factorial is: ", 0
+    newline db 0xA, 0
 
 section .bss
     input resb 4                       ; Reserve 4 bytes for input
@@ -10,71 +11,78 @@ section .text
 
 _start:
     ; Prompt user for input
-    mov eax, 4                         ; Syscall number for sys_write
-    mov ebx, 1                         ; File descriptor (stdout)
-    mov ecx, prompt                    ; Address of prompt message
-    mov edx, 14                        ; Length of prompt
-    int 0x80                           ; Make syscall
+    mov rax, 1                         ; Syscall number for sys_write
+    mov rdi, 1                         ; File descriptor (stdout)
+    mov rsi, prompt                    ; Address of prompt message
+    mov rdx, 14                        ; Length of prompt
+    syscall                            ; Make syscall
 
     ; Read user input
-    mov eax, 3                         ; Syscall number for sys_read
-    mov ebx, 0                         ; File descriptor (stdin)
-    mov ecx, input                     ; Address to store input
-    mov edx, 4                         ; Number of bytes to read
-    int 0x80                           ; Make syscall
+    mov rax, 0                         ; Syscall number for sys_read
+    mov rdi, 0                         ; File descriptor (stdin)
+    mov rsi, input                     ; Address to store input
+    mov rdx, 4                         ; Number of bytes to read
+    syscall                            ; Make syscall
 
     ; Convert input ASCII to integer
-    mov eax, [input]                   ; Load input into eax
-    sub eax, '0'                       ; Convert ASCII to integer
+    movzx rax, byte [input]            ; Load input into rax
+    sub rax, '0'                       ; Convert ASCII to integer
 
     ; Call factorial subroutine
-    push eax                           ; Push input number to stack as argument
+    push rax                           ; Push input number to stack as argument
     call factorial                     ; Call factorial function
-    add esp, 4                         ; Clean up argument from stack
+    add rsp, 8                         ; Clean up argument from stack
 
     ; Display result message
-    mov eax, 4                         ; Syscall number for sys_write
-    mov ebx, 1                         ; File descriptor (stdout)
-    mov ecx, result_msg                ; Address of result message
-    mov edx, 14                        ; Length of result message
-    int 0x80                           ; Make syscall
+    mov rax, 1                         ; Syscall number for sys_write
+    mov rdi, 1                         ; File descriptor (stdout)
+    mov rsi, result_msg                ; Address of result message
+    mov rdx, 14                        ; Length of result message
+    syscall                            ; Make syscall
 
-    ; Print the factorial result (stored in eax)
-    ; Assume eax contains the single-digit result for simplicity
-    add eax, '0'                       ; Convert integer result to ASCII
-    mov [input], eax                   ; Store ASCII result in input buffer
-    mov eax, 4                         ; Syscall number for sys_write
-    mov ebx, 1                         ; File descriptor (stdout)
-    mov ecx, input                     ; Address of result
-    mov edx, 1                         ; Length of result (1 digit)
-    int 0x80                           ; Make syscall
+    ; Print the factorial result (stored in rax)
+    ; Assume rax contains the single-digit result for simplicity
+    add rax, '0'                       ; Convert integer result to ASCII
+    mov [input], al                    ; Store ASCII result in input buffer
+    mov rax, 1                         ; Syscall number for sys_write
+    mov rdi, 1                         ; File descriptor (stdout)
+    mov rsi, input                     ; Address of result
+    mov rdx, 1                         ; Length of result (1 digit)
+    syscall                            ; Make syscall
+
+    ; Print newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
 
     ; Exit the program
-    mov eax, 1                         ; Syscall number for sys_exit
-    xor ebx, ebx                       ; Exit code 0
-    int 0x80                           ; Make syscall
+    mov rax, 60                        ; Syscall number for sys_exit
+    xor rdi, rdi                       ; Exit code 0
+    syscall                            ; Make syscall
 
 ; Factorial subroutine
 factorial:
     ; Prologue - save registers
-    push ebp                           ; Save base pointer
-    mov ebp, esp                       ; Set base pointer to current stack pointer
-    push ebx                           ; Save ebx register (used as counter)
+    push rbp                           ; Save base pointer
+    mov rbp, rsp                       ; Set base pointer to current stack pointer
+    push rbx                           ; Save rbx register (used as counter)
 
     ; Factorial calculation
-    mov eax, [ebp+8]                   ; Load the argument from the stack (n)
-    mov ebx, eax                       ; Copy n to ebx as a counter
-    dec ebx                            ; Decrement counter by 1
+    mov rax, [rbp+16]                  ; Load the argument from the stack (n)
+    mov rbx, rax                       ; Copy n to rbx as a counter
+    dec rbx                            ; Decrement counter by 1
 
     factorial_loop:
-        cmp ebx, 0                     ; Check if counter (n-1) is 0
+        cmp rbx, 0                     ; Check if counter (n-1) is 0
         je end_factorial               ; If yes, exit loop
-        mul ebx                        ; eax = eax * ebx
-        dec ebx                        ; Decrement counter
+        imul rax, rbx                  ; rax = rax * rbx
+        dec rbx                        ; Decrement counter
         jmp factorial_loop             ; Repeat loop
 
     end_factorial:
     ; Epilogue - restore registers
-    pop ebx                            ; Restore ebx register
-    pop ebp                            ; Restore base pointer
-    ret                                ; Return to caller with result in eax
+    pop rbx                            ; Restore rbx register
+    pop rbp                            ; Restore base pointer
+    ret                                ; Return to caller with result in rax
